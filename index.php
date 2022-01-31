0c46cc9e49eb6ae4738aef683ea52c611487ec75
Joan Sebastian Morales Pérez <josmoralesp0307@outlook.es>
2022-01-31 15:45:15 -0500

    Añadir y consultar funcionando

--- index.php
+++ index.php
@@ -8,9 +8,6 @@ include './view/home.php';
         <div class="col-md-6 p-5 text-center">
             <a href="./log/insertar.php" class="btn btn-primary">Registro Estudiante</a>
         </div>
-        <div class="col-md-6 p-5 text-center">
-            <Button class="btn btn-primary">Consulta Estudiantes</Button>
-        </div>
     </div>
 </body>
 
--- log/insertar.php
+++ log/insertar.php
@@ -3,7 +3,27 @@ include '../view/home.php';
 ?>
 
 <body>
+    <?php
+    if (isset($_POST['enviar'])) {
+        $nombre = $_POST['txtName'];
+        $apellido = $_POST['txtApellido'];
+        $fecha_nacimiento = $_POST['dateNacimiento'];
+        include("../db/conexion.php");
 
+        $sentencia = $bd->prepare("INSERT INTO estudiante(nombre, apellido, fecha_nacimiento) VALUES (?,?,?);");
+        $resultado = $sentencia->execute([$nombre, $apellido, $fecha_nacimiento]);
+        if ($resultado) {
+            echo "<script language='JavaScript'>
+                alert('Alumno agregado correctamente');
+                
+                </script>";
+        } else {
+            echo "<script language='JavaScript'>
+                alert('Error al guardar el alumno');
+                </script>";
+        }
+    }
+    ?>
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6 p-3">
@@ -11,20 +31,20 @@ include '../view/home.php';
                     <form class="m-4" action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
                         <div class="mb-3">
                             <label for="txtName" class="form-label">Nombre: </label>
-                            <input type="text" class="form-control" id="txtName">
+                            <input type="text" class="form-control" name="txtName">
                         </div>
                         <div class="mb-3">
                             <label for="txtApellido" class="form-label">Apellido: </label>
-                            <input type="text" class="form-control" id="txtApellido">
+                            <input type="text" class="form-control" name="txtApellido">
                         </div>
                         <div class="mb-3">
                             <label for="dateNacimiento" class="form-label">Fecha de nacimiento: </label>
-                            <input type="date" class="form-control" id="dateNacimiento">
+                            <input type="date" class="form-control" name="dateNacimiento">
                         </div>
-                        <button type="submit" class="btn btn-primary">Agregar</button>
-                    </form>
+                        <input type="submit" name="enviar" value="AGREGAR">
                     </form>
                 </div>
+                <a href="../index.php">Regresar</a>
             </div>
             <div class="col-md-6 p-3">
                 <?php
